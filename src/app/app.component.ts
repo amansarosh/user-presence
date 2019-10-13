ae75a1ff89bc5a4a7b8122ad2f395e97d3989d38
Aman Sarosh <amansarosh13@gmail.com>
2019-10-13 00:27:26 -0700

    User Presence added with Firebase, and Angular Material.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { AuthService } from './services/auth.service';
+import { PresenceService } from './services/presence.service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'firelerts';
+  constructor(public auth: AuthService, public presence: PresenceService) { }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,38 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { AngularFireModule } from '@angular/fire';
+
+import { AngularFireAuthModule } from '@angular/fire/auth';
+import { AngularFireDatabaseModule } from '@angular/fire/database';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from './material';
+
+const firebaseConfig = {
+  apiKey: "xxx",
+  authDomain: "firelerts.firebaseapp.com",
+  databaseURL: "https://firelerts.firebaseio.com",
+  projectId: "firelerts",
+  storageBucket: "firelerts.appspot.com",
+  messagingSenderId: "858396027735",
+  appId: "1:858396027735:web:2bafc0c5258c4e20f6ec12",
+  measurementId: "G-XSYJLNTJB0"
+};
+import { UserComponent } from './user/user.component';
+
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent, UserComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireAuthModule,
+    AngularFireDatabaseModule,
+    AngularFirestoreModule,
+    BrowserAnimationsModule,
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
